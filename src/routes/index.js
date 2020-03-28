18fd50342d3d52da2700552775a42dbddf0c7ccf
Felipe Araujo <devaraujofelipe@gmail.com>
2020-03-28 12:05:13 -0300

    Config routes

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import About from '~/pages/About';
+import Home from '~/pages/Home';
+import Work from '~/pages/Work';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/work" component={Work} />
+    </Switch>
+  );
+}
