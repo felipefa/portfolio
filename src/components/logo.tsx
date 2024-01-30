ab39aa7233125fd5332d01bd533df82b6ce6d64b
Felipe Araujo <devaraujofelipe@gmail.com>
2024-01-30 11:50:40 +0100

    style: adjust logo font size

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import { audiowide } from '@/shared/fonts';
 export function Logo() {
   return (
     <a
-      className={`${audiowide.className} hover:scale-125 text-5xl transition-transform`}
+      className={`${audiowide.className} hover:scale-125 text-3xl sm:text-4xl xl:text-5xl transition-transform`}
       href="#home"
     >
       <h1>Felipe</h1>
