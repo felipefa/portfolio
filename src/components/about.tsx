6147c772676b2df4dca6d08e4f497e469d5d3c16
Felipe Araujo <devaraujofelipe@gmail.com>
2024-01-31 22:50:42 +0100

    style: improve about section responsiveness

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -9,13 +9,13 @@ export function About() {
 
   return (
     <section
-      className="flex flex-1 flex-col items-center justify-center min-h-svh h-svh"
+      className="flex flex-1 flex-col xl:flex-row items-center justify-center min-h-svh h-svh"
       id="about"
     >
-      <p className="xl:hidden text-[rgba(var(--foreground-rgb),0.15)] font-display text-7xl mb-8">
+      <p className="xl:-rotate-90 text-[rgba(var(--foreground-rgb),0.15)] font-display text-7xl md:text-8xl mb-8 xl:mb-0">
         About
       </p>
-      <div className="border-[rgba(var(--foreground-rgb),0.15)] border-2 bg-[rgb(var(--background-rgb))] min-h-[50%] min-w-80 w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] max-w-[960px] rounded-2xl shadow-2xl shadow-[rgba(var(--foreground-rgb),0.15)] flex flex-col  overflow-hidden">
+      <div className="border-[rgba(var(--foreground-rgb),0.15)] border-2 bg-[rgb(var(--background-rgb))] min-h-80 h-[50%] xl:max-h-96 min-w-80 w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] max-w-[960px] rounded-2xl shadow-2xl shadow-[rgba(var(--foreground-rgb),0.15)] flex flex-col overflow-hidden xl:-ml-28">
         <div className="border-[rgba(var(--foreground-rgb),0.15)] border-b-2 flex items-center px-4 py-2">
           <X className="mr-2" />
           <Minus className="mr-2" />
