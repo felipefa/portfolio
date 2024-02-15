56521ba5de88cb8a9cfaf68ecc7f40362ed8c4b5
Felipe Araujo <devaraujofelipe@gmail.com>
2024-02-15 15:15:09 +0100

    style: change session title colour

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -3,7 +3,7 @@ import { projects } from '@/shared/data/projects';
 export function Projects() {
   return (
     <section className="flex flex-col min-h-svh pt-24 pb-8 gap-8" id="projects">
-      <h2 className="text-primary-500/15 text-7xl md:text-8xl font-display text-center">
+      <h2 className="text-primary-500 text-7xl md:text-8xl font-display text-center">
         Projects
       </h2>
       <div className="flex-1 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 px-8 max-w-6xl w-full mx-auto">
