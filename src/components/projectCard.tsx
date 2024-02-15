699e29d09943c0306ec925348aa8e60f6b1ea73e
Felipe Araujo <devaraujofelipe@gmail.com>
2024-02-15 15:29:58 +0100

    feat: add option to show all technologies of a project

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import React from 'react';
+
 import { Project } from '@/shared/data/projects';
 
 interface ProjectCardProps {
@@ -5,6 +9,20 @@ interface ProjectCardProps {
 }
 
 export function ProjectCard({ project }: ProjectCardProps) {
+  const [showAllTechs, setShowAllTechs] = React.useState(false);
+
+  const hasMoreThan3Techs = project.tech.length > 3;
+
+  const filteredTechs = React.useMemo(() => {
+    if (project.tech.length <= 3 || showAllTechs) return project.tech;
+
+    return project.tech.slice(0, 2);
+  }, [showAllTechs, project.tech]);
+
+  function handleShowAllTechs() {
+    setShowAllTechs(true);
+  }
+
   return (
     <div className="bg-purple-900/75 flex flex-col gap-4 p-6 rounded-xl hover:ring-2 hover:ring-primary-500 hover:shadow-2xl hover:shadow-primary-500/20 transition-all cursor-pointer">
       <div className="flex flex-col items-baseline">
@@ -14,14 +32,22 @@ export function ProjectCard({ project }: ProjectCardProps) {
         </p>
       </div>
       <div className="flex flex-1 items-start justify-start gap-2 flex-wrap">
-        {project.tech.slice(0, 3).map((tech) => (
+        {filteredTechs.map((tech) => (
           <p
-            className="text-sm font-medium min-w-fit py-1 px-1.5 rounded-full border border-primary-500"
+            className="text-sm font-medium min-w-fit py-1 px-1.5 rounded-full border border-primary-500 transition-colors hover:bg-primary-500 hover:text-purple-900"
             key={tech}
           >
             {tech}
           </p>
         ))}
+        {hasMoreThan3Techs && !showAllTechs && (
+          <button
+            className="text-sm font-medium min-w-fit py-1 px-1.5 rounded-full border border-primary-500 transition-colors hover:bg-primary-500 hover:text-purple-900"
+            onClick={handleShowAllTechs}
+          >
+            More...
+          </button>
+        )}
       </div>
     </div>
   );
