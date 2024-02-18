9a7f0c2862ed75aed6a59e9b998d1a76a442ff03
Felipe Araujo <devaraujofelipe@gmail.com>
2024-02-18 13:05:36 +0100

    feat: add description to project card

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -24,14 +24,17 @@ export function ProjectCard({ project }: ProjectCardProps) {
   }
 
   return (
-    <div className="bg-purple-900/75 flex flex-col gap-4 p-6 rounded-xl hover:ring-2 hover:ring-primary-500 hover:shadow-2xl hover:shadow-primary-500/20 transition-all cursor-pointer">
-      <div className="flex flex-col items-baseline">
-        <h3 className="font-display">{project.title}</h3>
-        <p className="font-bold text-xs text-primary-500/50">
+    <div className="bg-purple-900/50 flex flex-col gap-4 p-6 rounded-xl hover:ring-2 hover:ring-primary-500 hover:shadow-2xl hover:shadow-primary-500/20 transition-all cursor-pointer">
+      <div className="flex flex-1 flex-col items-baseline">
+        <time className="text-sm text-primary-500/90">
           {project.date.getFullYear()}
+        </time>
+        <h3 className="font-display text-xl">{project.title}</h3>
+        <p className="font-medium h-12 mt-4 text-primary-500/90 line-clamp-2 w-full">
+          {project.description}
         </p>
       </div>
-      <div className="flex flex-1 items-start justify-start gap-2 flex-wrap">
+      <div className="flex items-start justify-start gap-2 flex-wrap">
         {filteredTechs.map((tech) => (
           <p
             className="text-sm font-medium min-w-fit py-1 px-1.5 rounded-full border border-primary-500 transition-colors hover:bg-primary-500 hover:text-purple-900"
