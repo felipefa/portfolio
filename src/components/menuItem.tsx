304540cbe0801e44932473db57825b5972c13b27
Felipe Araujo <devaraujofelipe@gmail.com>
2024-03-07 12:44:06 +0100

    refactor: use Next.js Link for MenuItem

--- src/components/menuItem.tsx
+++ src/components/menuItem.tsx
@@ -1,13 +1,16 @@
+import Link, { LinkProps } from 'next/link';
 import { AnchorHTMLAttributes } from 'react';
 import { twMerge } from 'tailwind-merge';
+
+type MenuItemProps = LinkProps & AnchorHTMLAttributes<HTMLAnchorElement>;
 
 export function MenuItem({
   children,
   className = '',
   ...props
-}: AnchorHTMLAttributes<HTMLAnchorElement>) {
+}: MenuItemProps) {
   return (
-    <a
+    <Link
       className={twMerge(
         'flex hover:scale-110 pt-4 mx-12 text-xl transition-transform uppercase',
         className
@@ -15,6 +18,6 @@ export function MenuItem({
       {...props}
     >
       {children}
-    </a>
+    </Link>
   );
 }
